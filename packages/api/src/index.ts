bb3341a02b6afae9e570fefc9ff38bd979b3d8a9
caseypugh <me@caseypugh.com>
2022-08-23 11:57:14 -0400

    specify relative path to @bull-board/ui
    Production environments wont give you access to the fullpath for security reasons. Must use relative paths.

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,7 +1,6 @@
 import { BaseAdapter } from './queueAdapters/base';
 import { IServerAdapter } from '../typings/app';
 import { getQueuesApi } from './queuesApi';
-import path from 'path';
 import { appRoutes } from './routes';
 import { errorHandler } from './handlers/error';
 
@@ -13,12 +12,11 @@ export function createBullBoard({
   serverAdapter: IServerAdapter;
 }) {
   const { bullBoardQueues, setQueues, replaceQueues, addQueue, removeQueue } = getQueuesApi(queues);
-  const uiBasePath = path.dirname(eval(`require.resolve('@bull-board/ui/package.json')`));
-
+  const uiBasePath = "node_modules/@bull-board/ui"
   serverAdapter
     .setQueues(bullBoardQueues)
-    .setViewsPath(path.join(uiBasePath, 'dist'))
-    .setStaticPath('/static', path.join(uiBasePath, 'dist/static'))
+    .setViewsPath(uiBasePath + 'dist')
+    .setStaticPath('/static', uiBasePath + 'dist/static')
     .setEntryRoute(appRoutes.entryPoint)
     .setErrorHandler(errorHandler)
     .setApiRoutes(appRoutes.api);
